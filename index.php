12475e34c44ccaa0f1a9eb9476cfd3b71a3fa102
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-01-18 14:46:40 +0900

    bodyに追加

--- index.php
+++ index.php
@@ -10,6 +10,47 @@
 </head>
 
 <body>
+<h1 class="title">PHPで掲示板アプリ</h1>
+    <hr>
+    <div class="boardWrapper">
+        <!-- メッセージ送信成功時 -->
+        <?php if (!empty($success_message)) : ?>
+            <p class="success_message"><?php echo $success_message; ?></p>
+        <?php endif; ?>
+
+        <!-- バリデーションチェック時 -->
+        <?php if (!empty($error_message)) : ?>
+            <?php foreach ($error_message as $value) : ?>
+                <div class="error_message">※<?php echo $value; ?></div>
+            <?php endforeach; ?>
+        <?php endif; ?>
+        <section>
+            <?php if (!empty($message_array)) : ?>
+                <?php foreach ($message_array as $value) : ?>
+                    <article>
+                        <div class="wrapper">
+                            <div class="nameArea">
+                                <span>名前：</span>
+                                <p class="username"><?php echo $value['username'] ?></p>
+                                <time>：<?php echo date('Y/m/d H:i', strtotime($value['post_date'])); ?></time>
+                            </div>
+                            <p class="comment"><?php echo $value['comment']; ?></p>
+                        </div>
+                    </article>
+                <?php endforeach; ?>
+            <?php endif; ?>
+        </section>
+        <form method="POST" action="" class="formWrapper">
+            <div>
+                <input type="submit" value="書き込む" name="submitButton">
+                <label for="usernameLabel">名前：</label>
+                <input type="text" name="username">
+            </div>
+            <div>
+                <textarea name="comment" class="commentTextArea"></textarea>
+            </div>
+        </form>
+    </div>
 
 </body>
 
