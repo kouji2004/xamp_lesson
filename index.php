a4726facafaf0a411c30bffa1f64c6a542819635
Kouji Miura <100500134+kouji2004@users.noreply.github.com>
2023-01-18 14:42:27 +0900

    html cssファイル追加

--- index.php
+++ index.php
@@ -1,1 +1,18 @@
 <?php echo "hello PHP";
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP掲示板</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
